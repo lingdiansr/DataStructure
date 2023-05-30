54a6e8f4ea1344fd61230a99b97d300e3ad7fd92
lingdianshiren <2077358365@qq.com>
2023-05-30 13:13:48 +0800

    master

--- homework-1364/01.c
+++ homework-1364/01.c
@@ -3,12 +3,17 @@
 int n, *tree;
 int countnodes(int index)
 {
-    if (index <= n && tree[index])
+    if (2*index <= n && tree[index])
     {
         int leftChildNum = countnodes(2 * index);
         int rightChildNum = countnodes(2 * index + 1);
         return leftChildNum + rightChildNum+1;
     }
+    else if (tree[index])
+    {
+        return 1;
+    }
+    
     else
     {
         return 0;
